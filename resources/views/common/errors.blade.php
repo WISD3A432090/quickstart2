794029a6a7c2c9458a1a013c1e2af95055c975c7
M511-50\admin <s3a432090@student.ncut.edu.tw>
2017-10-19 16:11:12 +0800

    增加[任務

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/10/19
- * Time: 下午 04:00
- */
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
